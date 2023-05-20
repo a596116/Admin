d63cb48a9bd7fe555cf404c51f1b3905db291e42
haodai <a596116@gmail.com>
2023-05-20 14:53:03 +0800

    api

--- src/plugins/axios/index.ts
+++ src/plugins/axios/index.ts
@@ -4,6 +4,6 @@ import Axios from './Axios'
 export const http = new Axios({
   // baseURL: 'https://nestjs-a596116.vercel.app/',
   // baseURL: 'http://localhost:3000/',
-  baseURL: env.VITE_BASEURL,
+  baseURL: `${env.VITE_BASEURL}`,
   timeout: 5000,
 })
