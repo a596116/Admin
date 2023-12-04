d84716f9e023a655db06a22c3cc89ea0e93cf8da
haodai <haodai.wang@sshop.com.tw>
2023-12-04 15:06:25 +0800

    env

--- types/components.d.ts
+++ types/components.d.ts
@@ -43,7 +43,6 @@ declare module '@vue/runtime-core' {
     ElRadio: typeof import('element-plus/es')['ElRadio']
     ElRadioGroup: typeof import('element-plus/es')['ElRadioGroup']
     ElRow: typeof import('element-plus/es')['ElRow']
-    ElScrollbar: typeof import('element-plus/es')['ElScrollbar']
     ElSelect: typeof import('element-plus/es')['ElSelect']
     ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
     ElSwitch: typeof import('element-plus/es')['ElSwitch']
