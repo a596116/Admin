b856994a32d49ac90229222fa51b161d804ef98b
haodai <a596116@gmail.com>
2024-02-28 16:46:14 +0800

    up

--- vite.config.ts
+++ vite.config.ts
@@ -18,14 +18,14 @@ export default ({ command }: ConfigEnv): any => {
     build: {
       rollupOptions: {
         output: {
-          // entryFileNames: `assets/[name].js`,
-          // chunkFileNames: `assets/[name].js`,
-          // assetFileNames: `assets/[name].[ext]`,
-          manualChunks(id: string) {
-            if (id.includes('node_modules')) {
-              return id.toString().split('node_modules/')[1].split('/')[0].toString()
-            }
-          },
+          entryFileNames: `assets/[name].js`,
+          chunkFileNames: `assets/[name].js`,
+          assetFileNames: `assets/[name].[ext]`,
+          // manualChunks(id: string) {
+          //   if (id.includes('node_modules')) {
+          //     return id.toString().split('node_modules/')[1].split('/')[0].toString()
+          //   }
+          // },
         },
       },
       cssCodeSplit: true,
@@ -49,7 +49,7 @@ export default ({ command }: ConfigEnv): any => {
     },
     server: {
       port: 3069,
-      // base: "./ ", //生产环境路径
+      base: "./ ", //生产环境路径
       proxy: {
         '/api': {
           target: process.env.VITE_BASEURL,
